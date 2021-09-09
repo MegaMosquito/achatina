550ad82e4d2ac333d41f33de7631c8279c5e9437
Clement Ng <clementdng@gmail.com>
2021-09-08 17:12:29 -0700

    Remove unused RTN_JSON env variable from restcam
    Signed-off-by: Clement Ng <clementdng@gmail.com>

--- shared/restcam/cam.sh
+++ shared/restcam/cam.sh
@@ -8,7 +8,6 @@ if [ -z "${CAM_OUT_HEIGHT:-}" ]; then CAM_OUT_HEIGHT=480; fi
 
 # Files (@@@ these should all be just in RAM)
 JPG="/tmp/cam.jpg"
-RTN_JSON="/tmp/rtn.json"
 SCALE="${CAM_OUT_WIDTH}x${CAM_OUT_HEIGHT}"
 
 # Use this static image if anything goes wrong
