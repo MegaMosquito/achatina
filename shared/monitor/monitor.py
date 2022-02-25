b6fab5cf60b0f6c087cd32fda76716e5ac1df217
Clement Ng <clementdng@gmail.com>
2022-02-24 18:51:47 -0800

    Remove overwritten Cache-Control request header
    Signed-off-by: Clement Ng <clementdng@gmail.com>

--- shared/monitor/monitor.py
+++ shared/monitor/monitor.py
@@ -73,7 +73,6 @@ if __name__ == '__main__':
   # Prevent caching everywhere
   @webapp.after_request
   def add_header(r):
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
     r.headers["Pragma"] = "no-cache"
     r.headers["Expires"] = "0"
     r.headers["Cache-Control"] = "public, max-age=0"
