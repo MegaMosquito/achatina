58165cf699f9f7443489f8ac5ff3127e014ae8a3
Clement Ng <clementdng@gmail.com>
2022-03-16 22:13:08 -0700

    Refactor monitor to look more like a typical flask app
    Signed-off-by: Clement Ng <clementdng@gmail.com>

--- shared/monitor/monitor.py
+++ shared/monitor/monitor.py
@@ -12,6 +12,11 @@ import time
 from datetime import datetime
 import base64
 
+from io import BytesIO
+from flask import Flask
+from flask import send_file
+from flask import render_template
+
 # Configuration constants
 MQTT_SUB_COMMAND = 'mosquitto_sub -h mqtt -p 1883 -C 1 '
 MQTT_DETECT_TOPIC = '/detect'
@@ -22,76 +27,70 @@ DUMMY_DETECT_IMAGE='/dummy_detect.jpg'
 # Globals for the cached JSON data (last messages on these MQTT topics)
 last_detect = None
 
-if __name__ == '__main__':
+webapp = Flask('monitor')
+webapp.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-  from io import BytesIO
-  from flask import Flask
-  from flask import send_file
-  from flask import render_template
-  webapp = Flask('monitor')                             
-  webapp.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-
-  @webapp.route("/images/detect.jpg")
-  def get_detect_image():
-    if last_detect:
-      j = json.loads(last_detect)
-      i = base64.b64decode(j['detect']['image'])
-      buffer = BytesIO()
-      buffer.write(i)
-      buffer.seek(0)
-      return send_file(buffer, mimetype='image/jpg')
-    else:
-      return send_file(DUMMY_DETECT_IMAGE)
-
-  @webapp.route("/json")
-  def get_json():
-    if last_detect:
-      return last_detect.decode("utf-8") + '\n'
-    else:
-      return '{}\n'
-
-  @webapp.route("/")
-  def get_results():
-    if None == last_detect:
-      now = datetime.now().strftime("%Y/%m/%d %-I:%M%p")
-      return '{"error":"' + now + ' -- No data yet."}'
+@webapp.route("/images/detect.jpg")
+def get_detect_image():
+  if last_detect:
     j = json.loads(last_detect)
-    n = j['device-id']
-    c = len(j['detect']['entities'])
-    ct = j['detect']['cam-time']
-    it = j['detect']['inf-time']
-    s = j['source']
-    u = j['source-url']
-    # print(s, u)
-    kafka_msg = ' Nothing is being published to Kafka!\n'
-    if 'kafka-sub' in j:
-      sub = j['kafka-sub']
-      kafka_msg = ' This data is also being published to EventStreams (kafka). Subscribe with: <p style="font-family:monospace;">' + sub + '</p>\n'
-    context = {k:v for (k,v) in locals().copy().items() if k in ('s', 'n', 'c', 'ct', 'it', 'u', 'kafka_msg')}
-    return render_template("monitor.html", **context)
+    i = base64.b64decode(j['detect']['image'])
+    buffer = BytesIO()
+    buffer.write(i)
+    buffer.seek(0)
+    return send_file(buffer, mimetype='image/jpg')
+  else:
+    return send_file(DUMMY_DETECT_IMAGE)
 
-  # Prevent caching everywhere
-  @webapp.after_request
-  def add_header(r):
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    r.cache_control.max_age = 0
-    r.cache_control.public = True
-    return r
+@webapp.route("/json")
+def get_json():
+  if last_detect:
+    return last_detect.decode("utf-8") + '\n'
+  else:
+    return '{}\n'
 
-  # Loop forever collecting object detection / classification data from MQTT
-  def collect_data():
-    global last_detect
-    # print("\nMQTT \"" + MQTT_DETECT_TOPIC + "\" topic monitor thread started!")
-    DETECT_COMMAND = MQTT_SUB_COMMAND + '-t ' + MQTT_DETECT_TOPIC
-    while True:
-      last_detect = subprocess.check_output(DETECT_COMMAND, shell=True)
-      # print("\n\nMessage received on detect topic...\n")
-      # print(last_detect)
+@webapp.route("/")
+def get_results():
+  if None == last_detect:
+    now = datetime.now().strftime("%Y/%m/%d %-I:%M%p")
+    return '{"error":"' + now + ' -- No data yet."}'
+  j = json.loads(last_detect)
+  n = j['device-id']
+  c = len(j['detect']['entities'])
+  ct = j['detect']['cam-time']
+  it = j['detect']['inf-time']
+  s = j['source']
+  u = j['source-url']
+  # print(s, u)
+  kafka_msg = ' Nothing is being published to Kafka!\n'
+  if 'kafka-sub' in j:
+    sub = j['kafka-sub']
+    kafka_msg = ' This data is also being published to EventStreams (kafka). Subscribe with: <p style="font-family:monospace;">' + sub + '</p>\n'
+  context = {k:v for (k,v) in locals().copy().items() if k in ('s', 'n', 'c', 'ct', 'it', 'u', 'kafka_msg')}
+  return render_template("monitor.html", **context)
 
+# Prevent caching everywhere
+@webapp.after_request
+def add_header(r):
+  r.headers["Pragma"] = "no-cache"
+  r.headers["Expires"] = "0"
+  r.cache_control.max_age = 0
+  r.cache_control.public = True
+  return r
+
+# Loop forever collecting object detection / classification data from MQTT
+def collect_data():
+  global last_detect
+  # print("\nMQTT \"" + MQTT_DETECT_TOPIC + "\" topic monitor thread started!")
+  detect_command = MQTT_SUB_COMMAND + '-t ' + MQTT_DETECT_TOPIC
+  while True:
+    last_detect = subprocess.check_output(detect_command, shell=True)
+    # print("\n\nMessage received on detect topic...\n")
+    # print(last_detect)
+
+if __name__ == '__main__':
   # Main program (starts monitor thread and then web server)
   monitor_detect =  threading.Thread(target=collect_data)
   monitor_detect.start()
 
   webapp.run(host=FLASK_BIND_ADDRESS, port=FLASK_PORT)
-
